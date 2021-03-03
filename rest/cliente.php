e36227cb46781f9f8a8e8725198bcc5a02ff3cc7
Stefano <stefano.pettini@disc.it>
2021-03-03 07:13:34 +0100

    Aggiunto header

--- rest/cliente.php
+++ rest/cliente.php
@@ -4,5 +4,5 @@ require_once '../lib/helper.php';
 
 /** costante di classe nome tabella cliente */
 define("TABELLA_CLIENTE","clienti");
-
+header('Content-Type: application/json');
 echo fromDaoToDTO(getDaoByKey(TABELLA_CLIENTE, ""));
